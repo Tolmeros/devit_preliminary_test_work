cc2e698c8fb78307a7b6ee2f814473c6e0241baa
tolmeros@phoenix <pass>
2022-03-25 04:57:32 +0200

    Fixed the solution to the js task number 9

--- js/09_removeDuplicate.js
+++ js/09_removeDuplicate.js
@@ -1,28 +1,9 @@
-class myString extends String {
-    removeDuplicate() {
-        var value = this.valueOf();
-        const unique = Array.from(new Set(value.split(' ')));
-        return unique.join(' ');
-    }
-}
 
-let y = new myString('Int32 Int32 Int32 Int32 Int32 Int32 Int32 Int32 Int32 Double Double Double');
-
-console.log(y.removeDuplicate());
-/* output
-
-Int32 Double
-
-*/
-
-/*
 String.prototype.removeDuplicate = function() {
     var value = this.valueOf();
     const unique = Array.from(new Set(value.split(' ')));
     return unique.join(' ');
 };
-*/
-String.prototype.removeDuplicate = myString.prototype.removeDuplicate;
 
 let x = "Int32 Int32 Int32 Int32 Int32 Int32 Int32 Int32 Int32 Double Double Double";
 
